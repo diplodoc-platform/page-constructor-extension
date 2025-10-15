d1611cb0836c47f79fe9acd454ded4e1e4ee307d
d3m1d0v <d3m1d0v@yandex-team.ru>
2025-10-15 14:12:31 +0300

    fix(plugin): add constants re-export to node plugin

--- src/plugin/index-node.ts
+++ src/plugin/index-node.ts
@@ -1,6 +1,9 @@
 import {createPageConstructorContent} from '../renderer/server';
 import {setPageConstructorImplementation} from '../renderer/factory';
+import {ClassNames, PAGE_CONSTRUCTOR_RUNTIME, PAGE_CONSTRUCTOR_STYLE} from '../constants';
 
 setPageConstructorImplementation(createPageConstructorContent);
 
 export {transform} from './transform-node';
+export {PAGE_CONSTRUCTOR_RUNTIME, PAGE_CONSTRUCTOR_STYLE, ClassNames};
+export type {ConstructorBlock, PageContent} from '../types';
