f71c92b35c894eb22d0bd3abfd581642338c8e9d
d3m1d0v <d3m1d0v@yandex-team.ru>
2025-10-29 12:46:37 +0300

    fix(plugin): spot import from lodash

--- src/plugin/content-processing/link-resolver.ts
+++ src/plugin/content-processing/link-resolver.ts
@@ -1,8 +1,10 @@
 import type {PageContent} from '@gravity-ui/page-constructor';
 
-import {every, isArray, isString} from 'lodash';
-import {dirname, join, parse, relative, resolve} from 'path';
-import url from 'url';
+import every from 'lodash/every';
+import isArray from 'lodash/isArray';
+import isString from 'lodash/isString';
+import {dirname, join, parse, relative, resolve} from 'node:path';
+import url from 'node:url';
 
 import {isLocalUrl} from '../utils';
 
