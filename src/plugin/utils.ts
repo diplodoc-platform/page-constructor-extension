8553a4f5eef249d8dbd2a5d56526f8883d7ecc8b
d3m1d0v <d3m1d0v@yandex-team.ru>
2025-10-29 18:31:49 +0300

    refactor(plugin): move parsing and validation of yaml contents to utility function

--- src/plugin/utils.ts
+++ src/plugin/utils.ts
@@ -1,3 +1,27 @@
+import type {PageContent} from '@gravity-ui/page-constructor';
+
+import {load} from 'js-yaml';
+
+export type LoadPageContentResult =
+    | {success: true; content: PageContent}
+    | {success: false; error: string};
+export function loadPageContent(content: string): LoadPageContentResult {
+    const yamlContent = load(content.trimStart()) as PageContent;
+
+    if (!('blocks' in yamlContent)) {
+        const contentLines = content.trimStart().split('\n');
+        const firstLines = contentLines.slice(0, 3).join('\n');
+
+        const message =
+            `Page constructor content must have a "blocks:" property\n` +
+            `Content preview:\n${firstLines}${contentLines.length > 3 ? '\n...' : ''}`;
+
+        return {success: false, error: message};
+    }
+
+    return {success: true, content: yamlContent};
+}
+
 export function isLocalUrl(url: string) {
     return !/^(?:[a-z]+:)?\/\//i.test(url);
 }
